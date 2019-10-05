dc345592574ea67590e7e92d7179911e96eefd87
Alejandro Lopez <Alejandro.Lopez@m3eng.com>
2019-10-04 21:20:18 -0700

    First commit

---
+++ wwwroot/js/AJAX.js
@@ -0,0 +1,36 @@
+﻿var AJAX = (function () {
+    var set = function (ruta, modelo, cbSuccess, cbError, mthod) {
+        $.ajax({
+            type: "POST",//(mthod === undefined || mthod == null) ? "POST" : mthod
+            async: true,  
+            headers: 
+                { 'RequestVerificationToken': 
+                    $("input[name=__RequestVerificationToken]").val()
+                },
+            url: ruta
+            //,beforeSend: function (xhr) {
+            //    xhr.setRequestHeader("XSRF-TOKEN",
+            //        $('input:hidden[name="__RequestVerificationToken"]').val());
+            //},
+            //headers: {
+            //    RequestVerificationToken: 
+            //        $('input:hidden[name="__RequestVerificationToken"]').val()
+            //}
+            , data: JSON.stringify(modelo)// modelo !== undefined && modelo !== null && !$.isEmptyObject(modelo) ? JSON.stringify(modelo) : {}
+            , contentType: "application/json; charset=utf-8"
+            , responseType: "json"
+            , dataType: "json"
+            , success: cbSuccess
+            , error: cbError
+        });
+    }
+
+    var setDefault = function (ruta, modelo, cbSuccess) {
+        $.post(/*window.url +*/ ruta, modelo, cbSuccess);
+    }
+
+    return {
+        Set: set,
+        SetDefault: setDefault
+    }
+})();
